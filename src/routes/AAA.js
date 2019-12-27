8d5f7f43e716a4f6d8f424d1cb3df4bb7444c5b2
wuhao <asgardusk@gmail.com>
2019-12-27 15:13:39 +0800

    feat: 引入antd

--- src/routes/AAA.js
+++ src/routes/AAA.js
@@ -1,6 +1,8 @@
+
 import React, { Component } from 'react';
 import { Link } from 'dva/router';
-
+import { Button } from 'antd';
+ 
 class AAA extends Component {
   render() {
     return (
@@ -8,12 +10,20 @@ class AAA extends Component {
         <p>
           AAA页
         </p>
-        <Link to={'/aaa/bbb'}>去BBB页面</Link>
+        <Link to={'/aaa/bbb'}>
+          <Button type={'primary'} icon={'link'}>
+            去BBB页面
+          </Button>
+        </Link>
         <br />
-        <Link to={'/ccc'}>去CCC页面</Link>
+        <Link to={'/ccc'}>
+          <Button type={'default'} icon={'enter'}>
+            去CCC页面
+          </Button>
+        </Link>
       </div>
     );
   }
 }
-
+ 
 export default AAA;
